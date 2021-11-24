876d0cc9af78cf42f6e8825936b03a2a2ac04ad2
OnlyArjun <91238510+OnlyArjun@users.noreply.github.com>
2021-11-24 19:04:14 +0530

    newUpdate

--- vaccinationHelperUsingDictionaries.py
+++ vaccinationHelperUsingDictionaries.py
@@ -1,6 +1,28 @@
+
+# defining all the function required fot t
+def enterVaccination():
+    print("1")
+
+def getDetails():
+    print("2")
+
+def getTillNowDone():
+    print("3")
+
+# A startUp Interface for the user
 print(" \t!!!!! WELCOME TO THE VACCINATION PORTAL !!!! \n")
 
 print("\t\tWHAT DO YOU WANT TO PERFORM ? \n")
 print("\t      1. ENTER MY VACCINATION DETAILS          ")
 print("\t       2. GET MY VACCINATION DETAILS           ")
 print("     3. GET THE NUMBER OF VACCINATIONS DONE TILL NOW")
+
+# taking input from the user
+userChoices = int(input("\n\n\t     PLEASE ENTER YOUR SELECTION :- "))
+
+if(userChoices == 1):
+    enterVaccination()
+elif(userChoices == 2):
+    getDetails()
+else:
+    getTillNowDone()
