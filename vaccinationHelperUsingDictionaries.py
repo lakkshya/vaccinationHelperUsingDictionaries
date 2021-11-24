8a951cb598a3a29272eb0732555b7e4ea9ed0738
OnlyArjun <91238510+OnlyArjun@users.noreply.github.com>
2021-11-24 11:31:26 +0530

    doneWithIntro

--- vaccinationHelperUsingDictionaries.py
+++ vaccinationHelperUsingDictionaries.py
@@ -1,1 +1,6 @@
-print("Hello World !!")
+print(" \t!!!!! WELCOME TO THE VACCINATION PORTAL !!!! \n")
+
+print("\t\tWHAT DO YOU WANT TO PERFORM ? \n")
+print("\t      1. ENTER MY VACCINATION DETAILS          ")
+print("\t       2. GET MY VACCINATION DETAILS           ")
+print("     3. GET THE NUMBER OF VACCINATIONS DONE TILL NOW")
