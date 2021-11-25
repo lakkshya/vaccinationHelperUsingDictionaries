8ad55b3b65761a44aca3785481c3b569bd628fe3
Lakshya Rana <work.lakshyarana@gmail.com>
2021-11-25 09:52:50 +0530

    Update vaccinationHelperUsingDictionaries.py

--- vaccinationHelperUsingDictionaries.py
+++ vaccinationHelperUsingDictionaries.py
@@ -9,57 +9,68 @@ allDetails = {
 
 # defining all the function required fot t
 def enterVaccination():
-    print("\n\n")
-    print(" !!!! WELCOME TO DETAILS ADDITION PORTAL !!!! ")
+    print("\n")
+    print("\t   !!!! WELCOME TO DETAILS ADDITION PORTAL !!!! \n")
 
-    aadharNumber = input("\t PLEASE ENTER YOUR ADDHAR NUMBER :- ")
+    aadharNumber = input("\tPLEASE ENTER YOUR ADDHAR NUMBER :- ")
     while(len(aadharNumber) != 12):
         print("\t !!!! INVALID AADHAR NUMBER ENTERED PLEASE TRY AGAIN !!!! ")
         aadharNumber = input("\t PLEASE ENTER YOUR ADDHAR NUMBER :- ")
     
     if aadharNumber in allDetails:
-        print("\t YOUR DETAILS ARE ALREADY IN OUR RECORDS ")
+        print("\n\t YOUR DETAILS ARE ALREADY IN OUR RECORDS ")
         userConcern = input("\t WANT TO UPDATE SECOND DOSE INFO ( YES / NO ) :- ")
         if( userConcern == "YES"):
-            allDetails[aadharNumber]["DOSE 2"] = "OK"
+            allDetails[aadharNumber]["DOSE 2"] = "COMPLETED"
+        print("\n\t||    DETAILS OF SECOND DOSE ADDED SUCCESFULLY   ||")
     else:
-        name = input("\t PLEASE ENTER YOUR FULL NAME :- ")
-        gender = input("\t PLEASE SPECIFY YOUR GENDER :- ")
-        dobEntered = input("\t PLEASE ENTER YOUR DATE OF BIRTH :- ")
-        vaccinationType = input("\t PLEASE ENTER YOUR VACCINE TYPE / NAME :- ")
-        print("\t AS YOU ARE GETTING VACCINATED FIRST TIME YOU ARE ONLY ELIGIBLE FOR FIRST DOSE ")
-        allDetails[aadharNumber] = {"Name": name, "Gender" : gender, "D.O.B": dobEntered, "TYPE": vaccinationType, "DOSE 1": "OK", "DOSE 2": "NA"}
+        name = input("\tPLEASE ENTER YOUR FULL NAME :- ")
+        gender = input("\tPLEASE SPECIFY YOUR GENDER :- ")
+        dobEntered = input("\tPLEASE ENTER YOUR DATE OF BIRTH :- ")
+        vaccinationType = input("\tPLEASE ENTER YOUR VACCINE TYPE / NAME :- ")
+        print("\n\t|| YOU ARE GETTING VACCINATED FOR THE FIRST TIME ||")
+        print("\t||    DETAILS OF FIRST DOSE ADDED SUCCESFULLY    ||\n")
+        allDetails[aadharNumber] = {"Name": name, "Gender" : gender, "D.O.B": dobEntered, "TYPE": vaccinationType, "DOSE 1": "COMPLETED", "DOSE 2": "NA"}
 
 
 def getDetails():
-    print(allDetails["942790850424"])
+    showRecord = input("\n\tEnter your Aadhar Number to get details :- ")
+    print("\n\t\tName : ", allDetails[showRecord]["Name"])
+    print("\t\tGender : ", allDetails[showRecord]["Gender"])
+    print("\t\tDate of Birth : ", allDetails[showRecord]["D.O.B"])
+    print("\t\tVaccine Name : ", allDetails[showRecord]["TYPE"])
+    print("\t\tDose 1 : ", allDetails[showRecord]["DOSE 1"])
+    print("\t\tDose 2 : ", allDetails[showRecord]["DOSE 2"], "\n")
 
 def getTillNowDone():
-    print("\n\n")
+    print("\n")
     print(" NO OF PEOPLES GOT VACCINATED TILL NOW ARE :- ", len(allDetails)-1)
     noOfFirstDose = 0
     noOfSecondDose = 0
     for aadhars, values in allDetails.items():
         for keys in values:
-            if( keys == "DOSE 1" and values[keys] != "NA" ):
+            if( keys == "DOSE 1" and values[keys] == "COMPLETED" ):
                 noOfFirstDose = noOfFirstDose + 1
-            elif( keys == "DOSE 2" and values[keys] != "NA" ):
+            elif( keys == "DOSE 2" and values[keys] == "COMPLETED" ):
                 noOfSecondDose = noOfSecondDose + 1
     print(" NO OF PEOPLES GOT VACCINATED WITH I DOSE ARE :- ", noOfFirstDose)
     print(" NO OF PEOPLES GOT VACCINATED WITH II DOSE ARE :- ", noOfSecondDose)
         
 
 # A startUp Interface for the user
-print(" \t    !!!!! WELCOME TO THE VACCINATION PORTAL !!!! \n")
+print("--------------------------------------------------------------------")
+print("\n \t    !!!!! WELCOME TO THE VACCINATION PORTAL !!!! \n")
 
 while(True):
+    print("--------------------------------------------------------------------\n")
     print("\t\t   WHAT DO YOU WANT TO PERFORM ? \n")
     print("\t         1. ENTER MY VACCINATION DETAILS          ")
     print("\t          2. GET MY VACCINATION DETAILS           ")
     print("\t  3. GET THE NUMBER OF VACCINATIONS DONE TILL NOW")
     print("\t\t\t    4. EXIT")
+    print("\n--------------------------------------------------------------------")
 
-    userChoices = int(input("\n\n\t     PLEASE ENTER YOUR SELECTION :- "))
+    userChoices = int(input("\n\t     PLEASE ENTER YOUR SELECTION :- "))
 
     if(userChoices == 1):
         enterVaccination()
